c530999dc47d019eecf038ef6b6eb8c4369e27af
moham <mohammad.asif2008@hotmail.com>
2023-11-22 07:38:03 -0600

    first commit of the project mobileautomation

---
+++ src/test/java/org/selflearning/AppiumWarmup.java
@@ -0,0 +1,160 @@
+package org.selflearning;
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class AppiumWarmup extends CommonClass{
+
+    @Test
+    public void kickstartTest() throws MalformedURLException, InterruptedException {
+        WebElement el = driver.findElement(AppiumBy.accessibilityId("Preference"));
+        System.out.println("The value is: "+el.getText());
+        System.out.println("Is value clicked? "+el.isDisplayed());
+        el.click();
+        driver.findElement(AppiumBy.accessibilityId("3. Preference dependencies")).click();
+        driver.findElement(AppiumBy.id("android:id/checkbox")).click();
+        // For rotating the screen
+        DeviceRotation landScapeMode = new DeviceRotation(0,0,90);
+        driver.rotate(landScapeMode);
+        WebElement wifiSetting = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@resource-id=\"android:id/title\" and @text=\"WiFi settings\"]"));
+        Assert.assertEquals("WiFi settings", wifiSetting.getText());
+        wifiSetting.click();
+        //paste from clipboard
+        driver.setClipboardText("Mohammad WiFi");
+        driver.findElement(By.id("android:id/edit")).sendKeys(driver.getClipboardText());
+        driver.findElement(AppiumBy.id("android:id/button1")).click();
+
+    }
+    @Test @Ignore
+    public void longPress() throws InterruptedException {
+        boolean canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "left", 100, "top", 100, "width", 200, "height", 200,
+                "direction", "down",
+                "percent", 3.0,"duration",2000
+        ));
+        WebElement views = driver.findElement(AppiumBy.accessibilityId("Views"));
+        views.click();
+        driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
+        driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
+        WebElement longClick = driver.findElement(By.xpath("//android.widget.TextView[@text=\"People Names\"]"));
+        longClick.click();
+        Thread.sleep(5000);
+        //longClick.clear();
+
+
+        List<WebElement> elements = driver.findElements(By.className("android.widget.TextView"));
+        ArrayList<String> arrayOfElement = new ArrayList<>();
+        int size = elements.size();
+        System.out.println("Value of the list is: ");
+        for(int i=0; i<size;i++){
+            arrayOfElement.add(elements.get(i).getText());
+            System.out.println(elements.get(i).getText());
+        }
+        System.out.println("THIS IS SOME VALUE HERE: "+arrayOfElement.size());
+        for(int j=0; j<size;j++){
+            System.out.print(" "+arrayOfElement.get(j).toString());
+        }
+
+        WebElement longClick1 = driver.findElement(By.xpath("//android.widget.TextView[@text=\"People Names\"]"));
+        longPressOperation(longClick1);
+        WebElement sampleMenu = driver.findElement(By.xpath("//android.widget.TextView[@resource-id=\"android:id/title\"" +
+                "and @text=\"Sample menu\"]"));
+        Assert.assertEquals("Sample menu",sampleMenu.getText());
+        WebElement sampleAction = driver.findElement(By.xpath("//android.widget.TextView[@resource-id=\"android:id/title\"" +
+                "and @text=\"Sample action\"]"));
+        Assert.assertEquals("Sample action",sampleAction.getText());
+
+    }
+    @Test @Ignore
+    public void scrollOperation() throws InterruptedException {
+        WebElement views = driver.findElement(AppiumBy.accessibilityId("Views"));
+        views.click();
+        boolean isScrollable = false;
+        boolean check;
+        List<WebElement> elements;
+        Set<String> arrayOfElement = new HashSet<>();
+//        List<WebElement> elements = driver.findElements(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector())." +
+//                "scrollIntoView(text(\"WebView\"));"));
+        do{
+            elements = driver.findElements(AppiumBy.className("android.widget.TextView"));
+            Thread.sleep(10000);
+            int size = elements.size();
+            System.out.println("Value of the list is: "+size);
+            for(int i=0; i<size;i++){
+                arrayOfElement.add(elements.get(i).getText());
+                System.out.println(elements.get(i).getText());
+            }
+           check =isScrollable(isScrollable);
+           Thread.sleep(7000);
+        } while (check);
+        elements = driver.findElements(AppiumBy.className("android.widget.TextView"));
+        int size = elements.size();
+        for(int i=0; i<size;i++){
+            arrayOfElement.add(elements.get(i).getText());
+            System.out.println(elements.get(i).getText());
+        }
+
+
+     //   List<WebElement> elements = driver.findElements(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.TextView\");"));
+
+
+       int size1 = arrayOfElement.size();
+        System.out.println("THIS IS SOME VALUE HERE: "+arrayOfElement.size());
+        for(int j=0; j<size1;j++){
+            System.out.print(" "+arrayOfElement.size());
+        }
+    }
+    @Test @Ignore
+    public void swipeOperation() throws InterruptedException {
+        Thread.sleep(20000);
+        WebElement views = driver.findElement(AppiumBy.accessibilityId("Views"));
+        views.click();
+        WebElement gallery = driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Gallery\"]"));
+        Assert.assertEquals("Gallery",gallery.getText());
+        Assert.assertTrue(gallery.isDisplayed());
+        gallery.click();
+        WebElement photo = driver.findElement(AppiumBy.accessibilityId("1. Photos"));
+        Assert.assertEquals("1. Photos",photo.getText());
+        photo.click();
+        String el = driver.findElement(By.xpath("//android.widget.Gallery[@resource-id=\"io.appium.android.apis:id/gallery\"]/android.widget.ImageView[1]")).getAttribute("focusable");
+        Assert.assertEquals("true",el);
+        // Java
+        WebElement firstImage = driver.findElement(By.xpath("//android.widget.Gallery[@resource-id=\"io.appium.android.apis:id/gallery\"]/android.widget.ImageView[2]"));
+
+        Thread.sleep(20000);
+        swipeOperation(firstImage,"left");
+        String el1 = driver.findElement(By.xpath("//android.widget.Gallery[@resource-id=\"io.appium.android.apis:id/gallery\"]/android.widget.ImageView[1]")).getAttribute("focusable");
+
+        Assert.assertEquals(el1,"false");
+        Thread.sleep(20000);
+    }
+    @Test @Ignore
+    public void dragAndDropOperation() throws InterruptedException {
+        Thread.sleep(10000);
+        WebElement views = driver.findElement(AppiumBy.accessibilityId("Views"));
+        views.click();
+        driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+        WebElement from = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
+        int [] coordinate = {611,616};
+        dragDropOperation(from,coordinate);
+        Thread.sleep(6000);
+        String actual = driver.findElement(By.id("io.appium.android.apis:id/drag_result_text")).getText();
+        Assert.assertEquals(actual,"Dropped!");
+
+
+    }
+}
---
+++ src/test/java/org/selflearning/CommonClass.java
@@ -0,0 +1,122 @@
+package org.selflearning;
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class CommonClass {
+    public AndroidDriver driver;
+    public AppiumDriverLocalService service;
+    private String PATH = "C:\\Users\\moham\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+    private String APP = "C://Users//moham//IdeaProjects//Mobileautomation//src//test//java//resources//ApiDemos-debug.apk";
+    private String IP_ADDRESS = "127.0.0.1";
+    private String URL = "http://127.0.0.1:4723";
+    private String DEVICE_NAME = "Pixel2API29";
+    private int PORT_NUMBER = 4723;
+    @BeforeClass
+    public void startUp() throws MalformedURLException {
+
+        service = new AppiumServiceBuilder().withAppiumJS( new File(PATH)).withTimeout(Duration.ofMinutes(2))
+                .withIPAddress(IP_ADDRESS).usingPort(PORT_NUMBER).build();
+        service.start();
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName(DEVICE_NAME);
+        options.setApp(APP);
+        driver = new AndroidDriver(new URL(URL),options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+    }
+    @AfterClass
+    public void tearDown(){
+        if(driver!=null){
+            driver.quit();
+        }if(service != null){
+            service.stop();
+        }
+    }
+
+    /**
+     * The method is used to do a long press; meaning user needs to press the mouse on an element
+     * and keep it pressing until the desired view is displayed.
+     * @param longClick
+     */
+
+    public void longPressOperation(WebElement longClick){
+        ((JavascriptExecutor) driver).executeScript("mobile: longClickGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) longClick).getId(),"duration",2000));
+    }
+
+    /**
+     * The util method used for scrolling the page; It is used to scroll the entire page (pages)
+     * @param canScrollMore
+     * @return
+     */
+
+    public boolean isScrollable(boolean canScrollMore){
+                 canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "left", 100, "top", 100, "width", 200, "height", 200,
+                "direction", "down",
+                "percent", 3.0,"duration",2000
+        ));
+                 if(canScrollMore==true){
+                     return true;
+                 }else {
+                     return false;
+                 }
+    }
+
+    /**
+     * The util method scrolls to a specific element
+     * @param element
+     * @param direction
+     */
+    public void scrollOperations(WebElement element, String direction){
+        ((JavascriptExecutor)driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "elementId",((RemoteWebElement)element).getId(),"left", 100, "top", 100, "width", 200, "height", 200,
+                "direction", direction,
+                "percent", 3.0,"duration",2000));
+    }
+
+    /**
+     * The util method swipes to a specific element
+     * @param element
+     * @param direction
+     */
+    public void swipeOperation(WebElement element, String direction){
+        ((JavascriptExecutor)driver).executeScript("mobile: swipeGesture",ImmutableMap.of(
+                "elementId",((RemoteWebElement)element).getId(),
+                "direction",direction,
+                "percent", 0.75
+        ));
+    }
+    public void dragDropOperation(WebElement from, int [] to){
+        ((JavascriptExecutor)driver).executeScript("mobile: dragGesture",ImmutableMap.of(
+                "elementId",((RemoteWebElement)from).getId(),
+                "endX",to[0],
+                "endY",to[1]
+        ));
+    }
+    public WebElement fluentWaitForElement(WebDriver driver, By locator, Duration timeout, Duration pollingInterval) {
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(timeout)
+                .pollingEvery(pollingInterval)
+                .ignoring(org.openqa.selenium.NoSuchElementException.class);
+
+        return wait.until((ExpectedCondition<WebElement>) webDriver -> webDriver.findElement(locator));
+    }
+}
